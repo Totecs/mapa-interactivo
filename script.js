dd2f320453abe2b8677586dcca0eeb675bc3a87a
Totecs <tomasdelgadillo1602@gmail.com>
2026-03-31 19:38:49 -0300

    agregue marcador con popup

---
+++ script.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([-34.6037, -58.3816], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   attribution: 'OpenStreetMap'
+}).addTo(map);
+L.marker([-34.6037, -58.3816])
+   .addTo(map)
+   .bindPopup("Ubicación inicial")
+   .openPopup();
